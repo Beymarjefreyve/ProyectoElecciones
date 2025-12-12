8ee52b30520009d2afd499bf34ef60e252ab7e6c
beymarjefreyve <beymarvillamizar@gmail.com>
2025-12-12 04:02:17 -0500

    Add Async annotation to EmailService

--- src/main/java/com/universidad/elecciones/service/EmailService.java
+++ src/main/java/com/universidad/elecciones/service/EmailService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import jakarta.mail.internet.MimeMessage;
